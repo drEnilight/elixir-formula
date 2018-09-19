09335cfdeac3b4529cabcb2c92e30ccc578cb157
Vladislav Romaniuk <romanuk_v@live.ru>
2018-09-19 16:53:04 +0300

    update schema

--- db/schema.rb
+++ db/schema.rb
@@ -23,28 +23,4 @@ ActiveRecord::Schema.define(version: 2018_09_19_133114) do
     t.string "author_name"
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "provider", default: "email", null: false
-    t.string "uid", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.boolean "allow_password_change", default: false
-    t.datetime "remember_created_at"
-    t.integer "sign_in_count", default: 0, null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.string "current_sign_in_ip"
-    t.string "last_sign_in_ip"
-    t.string "first_name"
-    t.string "last_name"
-    t.string "email"
-    t.json "tokens"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-    t.index ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true
-  end
-
 end
