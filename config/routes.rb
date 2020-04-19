621451c232e6e40479bf2e24e4899948f9b78467
Vlad Romaniuk <romanuk_v@live.ru>
2020-04-19 12:09:06 +0300

    add BaseAuth for sidekiq

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 require 'sidekiq/web'
 
 Rails.application.routes.draw do
-  mount Sidekiq::Web => '/sidekiq'
+  scope :monitoring do
+    Sidekiq::Web.use Rack::Auth::Basic do |username, password|
+      ActiveSupport::SecurityUtils.secure_compare(::Digest::SHA256.hexdigest(username), ::Digest::SHA256.hexdigest(ENV["SIDEKIQ_AUTH_USERNAME"])) &
+        ActiveSupport::SecurityUtils.secure_compare(::Digest::SHA256.hexdigest(password), ::Digest::SHA256.hexdigest(ENV["SIDEKIQ_AUTH_PASSWORD"]))
+    end
+
+    mount Sidekiq::Web, at: '/sidekiq'
+  end
 end
