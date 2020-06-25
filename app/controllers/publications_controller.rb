ea53312a627e51c63e43dd68eb8c67cc2ce017de
Vlad Romaniuk <romanuk_v@live.ru>
2020-06-25 21:51:56 +0300

    add basic auth for publications page

--- app/controllers/publications_controller.rb
+++ app/controllers/publications_controller.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 class PublicationsController < ApplicationController
+  http_basic_authenticate_with name: ENV["SIDEKIQ_AUTH_USERNAME"],
+                               password: ENV["SIDEKIQ_AUTH_PASSWORD"]
+
   def index
     @publications = Publication.pending.order(created_at: :desc)
   end
